ac3d04cd3bfc1794616c253a224acb58a1338c49
Vinsento <hummanist@gmail.com>
2023-06-15 14:32:20 +0300

    add services

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -1,8 +1,28 @@
 import { getArgs } from "./helpers/args.js"
+import { printError, printHelp, printSuccess } from "./services/log.service.js"
+import { saveKeyValue } from "./services/storage.service.js"
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token)
+    printSuccess('Токен сохранен')
+  } catch(e) {
+    printError(e.message)
+  }
+}
 
 const initCLI = () => {
   const args = getArgs(process.argv)
-  console.log(args)
+  
+  if(args.h) {
+    printHelp()
+  }
+  if(args.s) {
+    saveKeyValue('town', args.s)
+  }
+  if(args.t) {
+    return saveToken(args.t)
+  }
 }
 
 initCLI()
